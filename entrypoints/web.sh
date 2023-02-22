a26f9abee840daec5bb4d6ef13a6fe424f91aa13
Ahmad Shafiq <mahmadshafiq786@gmail.com>
2023-02-22 18:24:46 +0500

    Setup till vector

--- entrypoints/web.sh
+++ entrypoints/web.sh
@@ -6,10 +6,13 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+echo " ----------> Running fluent-bit"
+/opt/fluent-bit/bin/fluent-bit -c fluent-bit.config
+
+echo " ----------> Running bundler"
+bundle check || bundle install  --jobs 4
 # echo " ----------> Running migrations"
 # bundle exec rails db:migrate
 
-echo " ----------> Running fluent-bit"
-/opt/fluent-bit/bin/fluent-bit -c fluent-bit.config
 echo " ----------> Running server"
 bundle exec rails s -b 0.0.0.0 webrick > /dev/null
